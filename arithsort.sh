5bac363ff57cd476a056a2fd50c0bcc1d697102a
amitkumaharana <ak657maharana@gmail.com>
2020-12-14 22:59:53 +0530

    Fourth Compute

--- arithsort.sh
+++ arithsort.sh
@@ -7,6 +7,8 @@ read c
 compute1=$((a+b*c))
 compute2=$((a*b+c))
 compute3=$((c+a/b))
+compute4=$((a%b+c))
 echo The result of first compute is $compute1
 echo The result of second compute is $compute2
 echo The result of third compute is $compute3
+echo The result of fourth compute is $compute4
