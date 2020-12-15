72d9730e3081c5fcf4b8e1df76fcda1f2263e7f1
amitkumaharana <ak657maharana@gmail.com>
2020-12-15 13:05:11 +0530

    Read the values from Dictionary into the Array

--- arithsort.sh
+++ arithsort.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
+#!/bin/bash -x
 declare -A dict
 echo "Welcome to the Arithmetic Computation & Sorting Program"
 echo "Enter 3 numbers"
 read a
 read b
 read c
-dict[compute1]=$((a+b*c))
-dict[compute2]=$((a*b+c))
-dict[compute3]=$((c+a/b))
-dict[compute4]=$((a%b+c))
-echo The result of all computes are ${dict[@]}
+dict[1]=$((a+b*c))
+dict[2]=$((a*b+c))
+dict[3]=$((c+a/b))
+dict[4]=$((a%b+c))
+echo ${dict[@]}
+for((j=0;j<${#dict[@]};j++))
+do
+	w=$((j+1))
+        array[j]=${dict[$w]}
+done
+echo The reasults of all computes are ${array[@]}
+echo ${dict[1]}
