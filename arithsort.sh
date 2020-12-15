2e5ca3c62a4161ebbb57bd24d7e2dbca84347a81
amitkumaharana <ak657maharana@gmail.com>
2020-12-15 12:24:07 +0530

    Stored results in a dictionary

--- arithsort.sh
+++ arithsort.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+declare -A dict
 echo "Welcome to the Arithmetic Computation & Sorting Program"
 echo "Enter 3 numbers"
 read a
 read b
 read c
-compute1=$((a+b*c))
-compute2=$((a*b+c))
-compute3=$((c+a/b))
-compute4=$((a%b+c))
-echo The result of first compute is $compute1
-echo The result of second compute is $compute2
-echo The result of third compute is $compute3
-echo The result of fourth compute is $compute4
+dict[compute1]=$((a+b*c))
+dict[compute2]=$((a*b+c))
+dict[compute3]=$((c+a/b))
+dict[compute4]=$((a%b+c))
+echo The result of all computes are ${dict[@]}
